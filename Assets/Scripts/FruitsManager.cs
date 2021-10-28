c4aa7b2afd4dfbf87dc8ac24607ee0aae003ca93
Franco <franco.mana@hotmail.com>
2021-10-28 00:12:31 -0300

    level 4; system points

--- Assets/Scripts/FruitsManager.cs
+++ Assets/Scripts/FruitsManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class FruitsManage : MonoBehaviour
+public class FruitsManager : MonoBehaviour
 {
     public Text clearLevel;
     public Animator transition;
+
+    public Text totalFruits;
+    public Text fruitsCollected;
+    private int totalFruitsInLevel;
+
+    private void Start()
+    {
+        totalFruitsInLevel = transform.childCount;//numero
+    }
+
     private void Update()
     {
         FruitsCollected();// pregunto todo el tiempo por si agarra +1 a la vez
+        totalFruits.text = totalFruitsInLevel.ToString();
+        fruitsCollected.text = transform.childCount.ToString();
     }
     public void FruitsCollected()
     {
