5b79214a85420af8cd27151933676028001abf15
Franco <franco.mana@hotmail.com>
2021-10-29 12:46:08 -0300

    Enemy Plant

---
+++ Assets/Scripts/Enemies/Plant/PlantEnemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantEnemy : MonoBehaviour
+{
+    private float waitedTime;
+    public float waitTimeToAttack = 3;
+    public Animator animator;
+    public GameObject bulletPrefabs;
+    public Transform launchSpawnPoint;
+
+    private void Start()
+    {
+        waitedTime = waitTimeToAttack;
+    }
+
+    private void Update()
+    {
+        if (waitedTime <= 0)
+        {
+            waitedTime = waitTimeToAttack;
+            animator.Play("Attack");
+            Invoke("LaunchBullet", 0.5f);
+        }
+        else
+            waitedTime -= Time.deltaTime;
+    }
+
+    public void LaunchBullet()
+    {
+        GameObject newBullet;
+        newBullet = Instantiate(bulletPrefabs, launchSpawnPoint.position, launchSpawnPoint.rotation);//spawn objeto
+    }
+}
