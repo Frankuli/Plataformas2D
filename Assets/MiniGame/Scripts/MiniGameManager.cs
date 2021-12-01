94b4ddf7afbc7681cabad381589d162653f81d70
Franco <franco.mana@hotmail.com>
2021-11-30 22:30:25 -0300

    Score

---
+++ Assets/MiniGame/Scripts/MiniGameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameManager : MonoBehaviour
+{
+    public Text scoreText;
+    public float scoreValue;
+
+    public Text maxScoreText;
+    public float maxScoreValue;
+
+    private void Start()
+    {
+        maxScoreValue = PlayerPrefs.GetFloat("Max");
+        maxScoreText.text = maxScoreValue.ToString("0.00");
+    }
+
+
+    void Update()
+    {
+        scoreValue += Time.deltaTime;
+
+        scoreText.text = scoreValue.ToString("0.00");
+
+        if (maxScoreValue < scoreValue)
+        {
+            PlayerPrefs.SetFloat("Max", scoreValue );
+            maxScoreText.text = scoreValue.ToString("0.00");
+        }
+
+    }
+}
