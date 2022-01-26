58e3e265ef336d24758b5a1c6633ec6d0ba3cee9
Franco <franco.mana@hotmail.com>
2022-01-26 13:15:28 -0300

    Difficulty

--- Assets/MiniGame/Scripts/SpawnObject.cs
+++ Assets/MiniGame/Scripts/SpawnObject.cs
@@ -11,20 +11,55 @@ public class SpawnObject : MonoBehaviour
     public Transform yUp;
     public Transform yDown;
 
+    public GameObject[] fruits;
+
+    public float timeSpawnFruits = 0.5f;
+    public float repeatSpawnRateFruits = 1.5f;
     public float timeSpawnEnemies = 1;
     public float repeatSpawnRateEnemies = 3;
 
-    public GameObject[] fruits;
-    public float timeSpawnFruits = 0.5f;
-    public float repeatSpawnRateFruits = 1.5f;
+    public float difficultyTime;
 
 
     void Start()
     {
-        InvokeRepeating("SpawnEnemies", timeSpawnEnemies, repeatSpawnRateEnemies);
-        InvokeRepeating("SpawnFruits", timeSpawnFruits, repeatSpawnRateFruits);
+        StartCoroutine("EnemyDifficulty");
+        StartCoroutine("FruitsDifficulty");
     }
 
+    private void Update()
+    {
+        difficultyTime += Time.deltaTime;
+
+        if (difficultyTime > 10 && difficultyTime < 20)
+        {
+            repeatSpawnRateEnemies = 2;
+            repeatSpawnRateFruits = 3;
+        }
+        if (difficultyTime > 20 && difficultyTime < 30)
+        {
+            repeatSpawnRateEnemies = 1;
+            repeatSpawnRateFruits = 4;
+        }
+        if (difficultyTime > 30 && difficultyTime < 50)
+            repeatSpawnRateEnemies = 0.75f;
+        if (difficultyTime > 50)
+            repeatSpawnRateEnemies = 0.5f;
+    }
+
+    IEnumerator EnemyDifficulty()
+    {
+        yield return new WaitForSeconds(repeatSpawnRateEnemies);
+        SpawnEnemies();
+        StartCoroutine("EnemyDifficulty");
+    }  
+    
+    IEnumerator FruitsDifficulty()
+    {
+        yield return new WaitForSeconds(repeatSpawnRateFruits);
+        SpawnFruits();
+        StartCoroutine("FruitsDifficulty");
+    }
     public void SpawnEnemies()
     {
         Vector3 spawnPosition = new Vector3(0, 0, 0);
